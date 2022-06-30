436e0d842dd7a9ebf8a80573a5e47454fbdc94e2
Dmitr-Kotov <71721108+Dmitr-Kotov@users.noreply.github.com>
2022-06-30 23:35:10 +0300

    Main class was created

---
+++ src/main/java/org/etu/practice/sort/visualizer/SortVisualizer.java
@@ -0,0 +1,8 @@
+package org.etu.practice.sort.visualizer;
+
+public class SortVisualizer {
+
+    public static void main(String[] argv) {
+        System.out.println("Start sort visualizer");
+    }
+}
