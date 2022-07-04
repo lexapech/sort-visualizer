c38515d3608a05501eb298cff3d5ae3970fe35aa
lexapech <lexa.pech@yandex.ru>
2022-07-04 17:21:08 +0300

    fixes

--- src/main/java/org/etu/practice/sort/visualizer/common/SortEnum.java
+++
@@ -1,7 +0,0 @@
-package org.etu.practice.sort.visualizer.common;
-
-public enum SortEnum {
-    BUBBLE_SORT,
-    QUICKSORT,
-    BITONIC_SORT
-}
--- src/main/java/org/etu/practice/sort/visualizer/gui/Canvas.java
+++ src/main/java/org/etu/practice/sort/visualizer/gui/Canvas.java
@@ -10,62 +10,52 @@ import java.util.List;
 import java.util.Random;
 
 public class Canvas extends JComponent {
-
     private Image image;
     private int barCount;
     private int[] array;
     private final List<Integer> accessed;
-    public Canvas()
-    {
-        image=new BufferedImage(1,1,BufferedImage.TYPE_INT_ARGB);
-        barCount=100;
-        accessed=new ArrayList<>();
+    public Canvas() {
+        image = new BufferedImage(1,1,BufferedImage.TYPE_INT_ARGB);
+        barCount = 100;
+        accessed = new ArrayList<>();
         array = new int[barCount];
         this.addComponentListener(new ComponentAdapter() {
             @Override
             public void componentResized(ComponentEvent e) {
-
                 Dimension newSize = e.getComponent().getSize();
                 image = new BufferedImage(newSize.width,newSize.height,BufferedImage.TYPE_INT_ARGB);
-                //System.out.println(e.getComponent().getSize().width);
                 super.componentResized(e);
                 repaint();
             }
         });
     }
 
-    private void drawBar(float height,int index)
-    {
+    private void drawBar(float height,int index) {
         Graphics g = image.getGraphics();
         g.setColor(Color.BLACK);
-        float barWidth= (float)image.getWidth(null)/barCount;
-        int heightInPixels=(int)(image.getHeight(null)*height);
+        float barWidth= (float)image.getWidth(null) / barCount;
+        int heightInPixels=(int)(image.getHeight(null) * height);
 
         if (accessed.contains(index))
             g.setColor(Color.RED);
         else
             g.setColor(Color.BLUE);
-        g.fillRect((int)(barWidth*index),image.getHeight(null)-heightInPixels,(int)barWidth+1,heightInPixels);
-       // g.setColor(Color.BLACK);
-        //g.drawRect((int)(barWidth*index),image.getHeight(null)-heightInPixels,(int)barWidth,heightInPixels);
+        g.fillRect((int)(barWidth * index),image.getHeight(null) - heightInPixels,(int)barWidth + 1, heightInPixels);
     }
     @Override
-    public void paintComponent(Graphics g)
-    {
+    public void paintComponent(Graphics g) {
         Random random = new Random();
         super.paintComponent(g);
-        for(int i=0;i<barCount;i++)
-        {
+        for(int i=0;i<barCount;i++) {
 
-            //drawBar(random.nextFloat(),i);
+
             drawBar(array[i]*1f/barCount,i);
         }
 
         g.drawImage(image,0,0,null);
 
     }
-    public void updateCanvas(int[] array)
-    {
+    public void updateCanvas(int[] array) {
         barCount=array.length;
         this.array=array;
         accessed.clear();
@@ -75,8 +65,7 @@ public class Canvas extends JComponent {
         repaint();
     }
 
-    public void markAccessed(int index)
-    {
+    public void markAccessed(int index) {
         accessed.add(index);
         repaint();
     }
