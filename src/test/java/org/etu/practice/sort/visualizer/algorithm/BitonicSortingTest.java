472af35cb1f0adf206d71b15f77010dcfb35c640
Nikolai <kola.ibatov@yandex.ru>
2022-07-06 11:18:16 +0300

    Added some classes for test + change report

---
+++ src/test/java/org/etu/practice/sort/visualizer/algorithm/BitonicSortingTest.java
@@ -0,0 +1,11 @@
+package org.etu.practice.sort.visualizer.algorithm;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class BitonicSortingTest {
+    @Test
+    public void test() {
+        Assertions.assertTrue(true);
+    }
+}
---
+++ src/test/java/org/etu/practice/sort/visualizer/algorithm/SortingAlgorithmTest.java
@@ -0,0 +1,11 @@
+package org.etu.practice.sort.visualizer.algorithm;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SortingAlgorithmTest {
+    @Test
+    public void test() {
+        Assertions.assertTrue(true);
+    }
+}
