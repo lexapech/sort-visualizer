e2f27a6ffae838dd394d9d2dd1a34fa756b7304a
Nikolai <kola.ibatov@yandex.ru>
2022-07-08 15:34:18 +0300

    little changes in SortVisualizer + added tests for algorithms

--- src/test/java/org/etu/practice/sort/visualizer/algorithm/BitonicSortingTest.java
+++ src/test/java/org/etu/practice/sort/visualizer/algorithm/BitonicSortingTest.java
@@ -1,11 +1,27 @@
 package org.etu.practice.sort.visualizer.algorithm;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.etu.practice.sort.visualizer.state.SortingState;
+import org.junit.jupiter.api.*;
+
+import java.util.Arrays;
 
 public class BitonicSortingTest {
+
+    private BitonicSorting<Integer> integerBitonicSorting;
+
+    @BeforeEach
+    void prepare() {
+        integerBitonicSorting = new BitonicSorting<>();
+    }
+
     @Test
-    public void test() {
-        Assertions.assertTrue(true);
+    public void sortArray() {
+        Integer[] sortArray = {1,3,4,6,2,7,5,8};
+        Integer[] answerArray = {1,2,3,4,5,6,7,8};
+        SortingState<Integer> initState
+                = new SortingState<>(sortArray, new int[0]);
+        integerBitonicSorting.sort(initState);
+        Integer[] resultArray = initState.sortingArray();
+        Assertions.assertArrayEquals(answerArray,resultArray);
     }
 }
--- src/test/java/org/etu/practice/sort/visualizer/algorithm/QuickSortingTest.java
+++ src/test/java/org/etu/practice/sort/visualizer/algorithm/QuickSortingTest.java
@@ -1,11 +1,27 @@
 package org.etu.practice.sort.visualizer.algorithm;
 
+import org.etu.practice.sort.visualizer.state.SortingState;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class QuickSortingTest {
+
+    private QuickSorting<Integer> integerQuickSorting;
+
+    @BeforeEach
+    void prepare() {
+        integerQuickSorting = new QuickSorting<>();
+    }
+
     @Test
-    public void test() {
-        Assertions.assertTrue(true);
+    public void sortArray() {
+        Integer[] sortArray = {5,8,2,7,3,1,6,9,4};
+        Integer[] answerArray = {1,2,3,4,5,6,7,8,9};
+        SortingState<Integer> initState
+                = new SortingState<>(sortArray, new int[0]);
+        integerQuickSorting.sort(initState);
+        Integer[] resultArray = initState.sortingArray();
+        Assertions.assertArrayEquals(answerArray,resultArray);
     }
 }
